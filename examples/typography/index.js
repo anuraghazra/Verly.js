5b449f7fac6d7d6510c2204a4b56ae459aaca942
anuraghazra <hazru.anurag@gmail.com>
2019-03-21 22:04:05 +0530

    added url params in ./typography

--- examples/typography/index.js
+++ examples/typography/index.js
@@ -4,20 +4,27 @@
  */
 let canvas = document.getElementById('c');
 let ctx = canvas.getContext('2d');
-let width = 900;
+let width = 1200;
 let height = 500;
 canvas.width = width;
 canvas.height = height;
 
 window.onload = function () {
-
   let verly = new Verly(50);
 
-  let word = new Text('VERLY', 200, 100, 25);
+  var url = new URL(window.location.href);
+  var query_string = url.search;
+  var search_params = new URLSearchParams(query_string);
+  var urlText = search_params.get('text') || 'HAPPY';
+  var urlText2 = search_params.get('text2') || 'HOLI';
+
+  let word = new Text(urlText, 50, 100, 25);
+  let word2 = new Text(urlText2, 700, 100, 25);
 
   // let A = new TypoGraphy(offsetX, center, 20, 'A');
 
   verly.addEntity(word.entity);
+  verly.addEntity(word2.entity);
 
   function animate() {
     ctx.clearRect(0, 0, width, height);
